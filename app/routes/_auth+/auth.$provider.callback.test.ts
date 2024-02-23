996978ea9eca5282a2753779c9e691bff9d23693
itsmegood <ishersingh16@gmail.com>
2024-02-23 08:21:05 +0530

    init

--- app/routes/_auth+/auth.$provider.callback.test.ts
+++ app/routes/_auth+/auth.$provider.callback.test.ts
@@ -142,7 +142,6 @@ test('gives an error if the account is already connected to another user', async
 	await prisma.user.create({
 		data: {
 			...createUser(),
-			platformStatusKey: 'ACTIVE',
 			connections: {
 				create: {
 					providerName: GITHUB_PROVIDER_NAME,
@@ -247,7 +246,6 @@ async function setupUser(userData = createUser()) {
 			user: {
 				create: {
 					...userData,
-					platformStatusKey: 'active',
 				},
 			},
 		},
